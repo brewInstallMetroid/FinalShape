f03508759ca8d23fda65a6b556ba4ca8bba168f9
brewInstallMetroid <christopherthompso@unomaha.edu>
2025-05-09 21:01:50 -0500

    DONE, hiding added, all logic good, docstrings finished

--- FunctionsArea.py
+++ FunctionsArea.py
@@ -1,72 +1,166 @@
 import math as m
 
-#TODO: Add/finish coding surface area and 3d shapes area functions AND ADD TYPE HINTING
 
-def circle(radius):
+def circle(radius) -> float:
+    """
+    This function calculates the area of the user's shape
+    if the user's shape is a circle
+    :param radius: the user's circle radius
+    :return: the area of the user's circle
+    """
     radius = float(radius)
     if radius <= 0:
-        raise TypeError
+        raise ValueError
     else:
         return m.pi * m.pow(radius, 2)
 
-def triangle(base, height):
+
+def triangle(base, height) -> float:
+    """
+    This function calculates the area of the user's shape
+    if the user's shape is a triangle
+    :param base: the user's triangle base
+    :param height: the user's triangle height
+    :return: the area of the user's triangle
+    """
     base = float(base)
     height = float(height)
     if base <= 0 or height <= 0:
-        raise TypeError
+        raise ValueError
     else:
         return (0.5 * base) * height
 
-def rectangle(x, y):
+
+def rectangle(x, y) -> float:
+    """
+    This function calculates the area of the user's shape
+    if the user's shape is a rectangle
+    :param x: the user's rectangle width
+    :param y: the user's rectangle height
+    :return: the area of the user's rectangle
+    """
     x = float(x)
     y = float(y)
     if x <= 0 or y <= 0:
-        raise TypeError
+        raise ValueError
     else:
         return x * y
 
-def square(x):
-     x = float(x)
-     if x <= 0:
-         raise TypeError
-     else:
+
+def square(x) -> float:
+    """
+    This function calculates the area of the user's shape
+    if the user's shape is a square
+    :param x: the user's square width
+    :return: the area of the user's square
+    """
+    x = float(x)
+    if x <= 0:
+        raise ValueError
+    else:
         return m.pow(x, 2)
 
 
-def rectangular_prism(x, y, z):
+def rectangular_prism(x, y, z) -> float:
+    """
+    This function calculates the area of the user's shape
+    if the user's shape is a rectangular prism
+    :param x: the user's rect. prism width
+    :param y: the user's rect. prism length
+    :param z: the user's rect. prism height
+    :return: the area of the user's rectangular prism
+    """
     x = float(x)
     y = float(y)
     z = float(z)
-    return 1
+    if x <= 0 or y <= 0 or z <= 0:
+        raise ValueError
+    else:
+        return x * y * z
 
-def rectangular_prism_surface_area(x, y, z):
+
+def rectangular_prism_surface_area(x, y, z) -> float:
+    """
+    This function calculates the surface area of the user's shape
+    if the user's shape is a rectangular prism
+    :param x: the user's rect. prism width
+    :param y: the user's rect. prism length
+    :param z: the user's rect. prism height
+    :return: the surface area of the user's rectangular prism
+    """
     x = float(x)
     y = float(y)
     z = float(z)
-    return 1
+    if x <= 0 or y <= 0 or z <= 0:
+        raise ValueError
+    else:
+        return x * y * 4 + y * z * 2
 
 
-def cone(radius, height, slant):
+def cone(radius, height, slant) -> float:
+    """
+    This function calculates the area of the user's shape
+    if the user's shape is a cone
+    :param radius: the user's cone radius
+    :param height: the user's cone height
+    :param slant: the user's cone slant height (unused here)
+    :return: the area of the user's cone
+    """
+    radius = float(radius)
+    height = float(height)
+    if radius <= 0 or height <= 0:
+        raise ValueError
+    else:
+        return (1/3) * m.pi * m.pow(radius, 2) * height
+
+
+def cone_surface_area(radius, height, slant) -> float:
+    """
+    This function calculates the surface area of the user's shape
+    if the user's shape is a cone
+    :param radius: the user's cone radius
+    :param height: the user's cone height
+    :param slant: the user's cone slant height
+    :return: the surface area of the user's cone
+    """
     radius = float(radius)
     height = float(height)
     slant = float(slant)
-    return 1
+    if radius <= 0 or height <= 0 or slant <= 0:
+        raise ValueError
+    else:
+        return m.pi * m.pow(radius, 2) + m.pi * radius * slant
 
-def cone_surface_area(radius, height, slant):
-    radius = float(radius)
+
+def pyramid(base, height, slant) -> float:
+    """
+    This function calculates the area of the user's shape
+    if the user's shape is a pyramid
+    :param base: the user's pyramid base length
+    :param height: the user's pyramid height
+    :param slant: the user's pyramid slant height (unused here)
+    :return: the area of the user's pyramid
+    """
+    base = float(base)
     height = float(height)
-    slant = float(slant)
-    return 1
+    if base <= 0 or height <= 0:
+        raise ValueError
+    else:
+        return (1/3) * m.pow(base, 2) * height
 
-
-def pyramid(base, height, slant):
+def pyramid_surface_area(base, height, slant) -> float:
+    """
+    This function calculates the surface area of the user's shape
+    if the user's shape is a pyramid
+    :param base: the user's pyramid base length
+    :param height: the user's pyramid height
+    :param slant: the user's pyramid slant height
+    :return: the surface area of the user's pyramid
+    """
     base = float(base)
     height = float(height)
     slant = float(slant)
-    return 1
-
-def pyramid_surface_area(base, height, slant):
-    base = float(base)
-    height = float(height)
-    slant = float(slant)
-    return 1
+    if base <= 0 or height <= 0 or slant <= 0:
+        raise ValueError
+    else:
+        return m.pow(base, 2) + 2 * base * slant
--- LogicArea.py
+++ LogicArea.py
@@ -2,11 +2,14 @@ from PyQt6.QtWidgets import *
 from GuiArea import *
 from FunctionsArea import *
 
-#TODO: Add type hinting, test new FunctionsArea functions, maybe add hiding if time allows
-
 class Logic (QMainWindow, Ui_MainWindow):
     value = 0
-    def __init__(self):
+    def __init__(self) -> None:
+        """
+        This function sets up the Gui and sets the Gui's
+        labels to their defaults
+        :return: None
+        """
         super().__init__()
         self.setupUi(self)
 
@@ -14,19 +17,36 @@ class Logic (QMainWindow, Ui_MainWindow):
         self.label_number2.setText("")
         self.label_number3.setText("")
         self.label_compute.setText("")
+        self.label_output_2.setText("Output:")
 
-    def clear_inputs(self):
+    def clear_inputs(self) -> None:
+        """
+        This function clears the number input boxes
+        :return: None
+        """
         self.input_number1.clear()
         self.input_number2.clear()
         self.input_number3.clear()
 
-    def select(self):
+    def select(self) -> None:
+        """
+        This function handles the logic for selecting the different
+        radio buttons and setting the text for the
+        number input boxes accordingly
+        :return: None
+        """
         if self.radio_circle.isChecked():
             Logic.value = 1
             self.clear_inputs()
             self.label_number1.setText("Radius")
             self.label_number2.setText("")
             self.label_number3.setText("")
+            self.input_number2.hide()
+            self.input_number3.hide()
+            self.radio_surface_area.hide()
+            self.radio_area.hide()
+            self.label_area_or_surface_area.hide()
+            self.button_compute.setText("3. COMPUTE")
 
         elif self.radio_square.isChecked():
             Logic.value = 2
@@ -34,6 +54,12 @@ class Logic (QMainWindow, Ui_MainWindow):
             self.label_number1.setText("Side")
             self.label_number2.setText("")
             self.label_number3.setText("")
+            self.input_number2.hide()
+            self.input_number3.hide()
+            self.radio_surface_area.hide()
+            self.radio_area.hide()
+            self.label_area_or_surface_area.hide()
+            self.button_compute.setText("3. COMPUTE")
 
         elif self.radio_rectangle.isChecked():
             Logic.value = 3
@@ -41,6 +67,12 @@ class Logic (QMainWindow, Ui_MainWindow):
             self.label_number1.setText("Length")
             self.label_number2.setText("Width")
             self.label_number3.setText("")
+            self.input_number2.show()
+            self.input_number3.hide()
+            self.radio_surface_area.hide()
+            self.radio_area.hide()
+            self.label_area_or_surface_area.hide()
+            self.button_compute.setText("3. COMPUTE")
 
         elif self.radio_triangle.isChecked():
             Logic.value = 4
@@ -48,6 +80,12 @@ class Logic (QMainWindow, Ui_MainWindow):
             self.label_number1.setText("Base")
             self.label_number2.setText("Height")
             self.label_number3.setText("")
+            self.input_number2.show()
+            self.input_number3.hide()
+            self.radio_surface_area.hide()
+            self.radio_area.hide()
+            self.label_area_or_surface_area.hide()
+            self.button_compute.setText("3. COMPUTE")
 
         elif self.radio_rectangular_prism.isChecked():
             Logic.value = 5
@@ -55,6 +93,14 @@ class Logic (QMainWindow, Ui_MainWindow):
             self.label_number1.setText("Length")
             self.label_number2.setText("Width")
             self.label_number3.setText("Height")
+            self.input_number2.show()
+            self.input_number3.show()
+            self.radio_surface_area.show()
+            self.radio_area.show()
+            self.radio_area.setText("Volume")
+            self.label_area_or_surface_area.show()
+            self.label_area_or_surface_area.setText("3. Would you like volume or surface area?")
+            self.button_compute.setText("4. COMPUTE")
 
         elif self.radio_cone.isChecked():
             Logic.value = 6
@@ -62,15 +108,38 @@ class Logic (QMainWindow, Ui_MainWindow):
             self.label_number1.setText("Radius")
             self.label_number2.setText("Height")
             self.label_number3.setText("Slant")
+            self.input_number2.show()
+            self.input_number3.show()
+            self.radio_surface_area.show()
+            self.radio_area.show()
+            self.radio_area.setText("Volume")
+            self.label_area_or_surface_area.show()
+            self.label_area_or_surface_area.setText("3. Would you like volume or surface area?")
+            self.button_compute.setText("4. COMPUTE")
+
 
         elif self.radio_pyramid.isChecked():
             Logic.value = 7
             self.clear_inputs()
-            self.label_number1.setText("Base Width")
+            self.label_number1.setText("Base")
             self.label_number2.setText("Height")
             self.label_number3.setText("Slant")
+            self.input_number2.show()
+            self.input_number3.show()
+            self.radio_surface_area.show()
+            self.radio_area.show()
+            self.radio_area.setText("Volume")
+            self.label_area_or_surface_area.show()
+            self.label_area_or_surface_area.setText("3. Would you like volume or surface area?")
+            self.button_compute.setText("4. COMPUTE")
 
-    def shape_calc(self):
+
+    def shape_calc(self) -> float:
+        """
+        This function deals with computing the end result of the
+        user's input by using functions in the FunctionsArea file
+        :return: The area or surface area of the user's shape
+        """
         if Logic.value == 1:
             return circle(self.input_number1.text())
         elif Logic.value == 2:
@@ -95,7 +164,14 @@ class Logic (QMainWindow, Ui_MainWindow):
             elif Logic.value == 7:
                 return pyramid_surface_area(self.input_number1.text(), self.input_number2.text(), self.input_number3.text())
 
-    def submit(self):
+    def submit(self) -> None:
+        """
+        This function calls the shape_calc() function and contains
+        the logic needed to output the correct phrases according to
+        the user's shape.  It also clears all the inputs and is
+        attached to the calculate button.
+        :return: None
+        """
         area_type = ''
         if self.radio_surface_area.isChecked():
             area_type = "Surface Area"
@@ -103,20 +179,22 @@ class Logic (QMainWindow, Ui_MainWindow):
             area_type = "Area"
         try:
             if Logic.value == 1:
-                self.label_compute.setText(f"Circle {area_type} = {self.shape_calc()}")
+                self.label_compute.setText(f"Circle {area_type} = {self.shape_calc():.3f}")
             elif Logic.value == 2:
-                self.label_compute.setText(f"Square {area_type} = {self.shape_calc()}")
+                self.label_compute.setText(f"Square {area_type} = {self.shape_calc():.3f}")
             elif Logic.value == 3:
-                self.label_compute.setText(f"Rectangle {area_type} = {self.shape_calc()}")
+                self.label_compute.setText(f"Rectangle {area_type} = {self.shape_calc():.3f}")
             elif Logic.value == 4:
-                self.label_compute.setText(f"Triangle {area_type} = {self.shape_calc()}")
+                self.label_compute.setText(f"Triangle {area_type} = {self.shape_calc():.3f}")
             elif Logic.value == 5:
-                self.label_compute.setText(f"Rect. Prism {area_type} = {self.shape_calc()}")
+                self.label_compute.setText(f"Rect. Prism {area_type} = {self.shape_calc():.3f}")
             elif Logic.value == 6:
-                self.label_compute.setText(f"Cone {area_type} = {self.shape_calc()}")
+                self.label_compute.setText(f"Cone {area_type} = {self.shape_calc():.3f}")
             elif Logic.value == 7:
-                self.label_compute.setText(f"Pyramid {area_type} = {self.shape_calc()}")
+                self.label_compute.setText(f"Pyramid {area_type} = {self.shape_calc():.3f}")
+            self.label_output_2.setText("Output:")
         except:
-            self.label_output_2.setText("Please enter numeric, positive values.")
+            self.label_output_2.setText("Numeric, positive values ONLY.")
+            self.label_compute.setText("")
 
         self.clear_inputs()
