4997640de6057507a4acfdf47bbf15ef04816539
brewInstallMetroid <christopherthompso@unomaha.edu>
2025-05-09 21:01:58 -0500

    DONE, hiding added, all logic good, docstrings finished

--- MainArea.py
+++ MainArea.py
@@ -1,6 +1,10 @@
 from LogicArea import *
 
-def main():
+def main() -> None:
+    '''
+    This function sets up the Gui
+    :return: None
+    '''
     application = QApplication([])
     window = Logic()
     window.show()
